033c5e413e6ada81c9b9f0be2d953ca224d3214e
Nozomu Onuma <nozomu.onuma@gmail.com>
2017-10-06 12:20:14 +0000

    add trace module 2

--- scripts/wall_trace.py
+++ scripts/wall_trace.py
@@ -39,7 +39,7 @@ class WallTrace():
       rate.sleep()
 
 if __name__ == '__main__':
-  rospy.init_node('wall_stop_accel')
+  rospy.init_node('wall_trace')
   rospy.wait_for_service('/motor_on')
   rospy.wait_for_service('/motor_off')
   rospy.on_shutdown(rospy.ServiceProxy('/motor_off',Trigger).call)
